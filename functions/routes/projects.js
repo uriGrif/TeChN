eb98c513c91f3239aabb89d11615499171efb142
unknown <urielgrif@gmail.com>
2021-05-07 19:24:24 -0300

    project router completo

--- functions/routes/projects.js
+++ functions/routes/projects.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose');
 const Project = require('../models/project')
+const Information = require('../models/information')
+const Requirement = require('../models/requirement')
+const UseCase = require('../models/useCase')
 
 router.use(express.json())
+
+const defaultContent = {
+    blocks: [
+        {
+            key: "si0j",
+            text: "Click your text to start editing and click outside to get out of edit mode. Remember to save your work",
+            type: "unstyled",
+            depth: 0,
+            inlineStyleRanges: [],
+            entityRanges: [],
+            data: {}
+        }
+    ],
+    entityMap: {}
+}
 
 router.get('/', (req, res) => {
     Project.find()
@@ -24,6 +43,28 @@ router.post('/add', (req, res) => {
     proj.save()
         .then(result => {
             res.send(result)
+            const projId = result._id;
+            const info = new Information(
+                {
+                    content: defaultContent,
+                    projectId: mongoose.Types.ObjectId(projId)
+                }
+            )
+            const requirement = new Requirement(
+                {
+                    content: defaultContent,
+                    projectId: mongoose.Types.ObjectId(projId)
+                }
+            )
+            const useCase = new UseCase(
+                {
+                    content: defaultContent,
+                    projectId: mongoose.Types.ObjectId(projId)
+                }
+            )
+            info.save()
+            requirement.save()
+            useCase.save()
         })
         .catch(err => {
             console.log(err)
