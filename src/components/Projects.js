df2eb7d94046f2357434cc0b14d2da5a018145ab
unknown <urielgrif@gmail.com>
2021-05-13 21:20:41 -0300

    cambie ubicacion imagen fondo y corregi get projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,52 +1,49 @@
-import { useState, useEffect } from 'react';
-import './ProjectDashboard.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import '../App.css';
-import CustomBtn from './CustomBtn';
-import Project from './Project';
-
+import { useState, useEffect } from "react";
+import "./ProjectDashboard.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../App.css";
+import CustomBtn from "./CustomBtn";
+import Project from "./Project";
 
 const Projects = () => {
+	const getProjects = async () => {
+		try {
+			const res = await fetch(".netlify/functions/api/projects");
+			const data = await res.json();
+			setProjects(data);
+		} catch (err) {
+			console.error("There has been an error loading the projects: " + err);
+		}
+	};
 
-    const getProjects = async () => {
-        try {
-            const res = await fetch('http://localhost:8000/projects');
-            const data = await res.json();
-            setProjects(data);
-        }
-        catch (err) {
-            console.error('There has been an error loading the projects: ' + err);
-        }
-    }
+	const [projects, setProjects] = useState([]);
 
-    const [projects, setProjects] = useState([])
+	useEffect(() => {
+		getProjects();
+	}, []);
 
-    useEffect(() => {
-        getProjects();
-    }, [])
-
-    return (
-        <div className="project">
-            <div className="titlePanel">
-                <h1 className="projectName">Projects</h1>
-            </div>
-            <div className="contenedor" style={{ minHeight: "50vh" }}>
-                <Link to="/new-project">
-                    <CustomBtn btnText="Add new project" />
-                </Link>
-                {
-                    projects.map((item) => {
-                        return (
-                            <Link key={item._id} to={`/project/${item.name}/${item._id}/information`} >
-                                <Project text={item.name}></Project>
-                            </Link>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
-
+	return (
+		<div className="project">
+			<div className="titlePanel">
+				<h1 className="projectName">Projects</h1>
+			</div>
+			<div className="contenedor" style={{ minHeight: "50vh" }}>
+				<Link to="/new-project">
+					<CustomBtn btnText="Add new project" />
+				</Link>
+				{projects.map(item => {
+					return (
+						<Link
+							key={item._id}
+							to={`/project/${item.name}/${item._id}/information`}
+						>
+							<Project text={item.name}></Project>
+						</Link>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
 
 export default Projects;
